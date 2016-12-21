6f3e4d3c4bf60ebd4a349df91bebe589828b2662
Tobias Weis <weis@ccc.cs.uni-frankfurt.de>
2016-12-21 02:18:17 +0100

    Added About-Activity to settings

---
+++ app/src/main/java/com/example/weis/cv_grabber/AboutActivity.java
@@ -0,0 +1,28 @@
+package com.example.weis.cv_grabber;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+/**
+ * Created by weis on 21.12.16.
+ */
+
+public class AboutActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        TextView tv = (TextView) findViewById(R.id.textView_about);
+        tv.setText(
+                Html.fromHtml("<h1>Multisensor Grabber</h1><br>" +
+                        "<p>Developer: Tobias Weis (<a href=\"mailto:mail@tobias-weis.de\">mail@tobias-weis.de</a>), " +
+                        "<a href=\"http://www.tobias-weis.de\">www.tobias-weis.de</a><br><br>" +
+                        "This app has been written to grab geo-annotated sequences for computer-vision experiments." +
+                        "Feel free to modify the code and use it for your work (no paper or report yet to cite):<br>" +
+                        "<a href=\"https://github.com/TobiasWeis/multisensorgrabber\">https://github.com/TobiasWeis/multisensorgrabber</a></p>")
+        );
+    }
+}
